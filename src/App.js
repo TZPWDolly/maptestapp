499b95c1c29b4663a2bb1e6b8ccf30bca2625872
TZPWDolly <dollydollytzpw98@gmail.com>
2021-09-22 16:32:33 +0630

    basic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+/* import logo from './logo.svg';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css';
+import './App.css'; */
 
-function App() {
+/* function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} className="leaflet-container">
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
+ */
 
+import { React, useState } from 'react'
+import './App.css'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { Icon } from 'leaflet'
+import covidData from './data.json'
+
+function App() {
+  const [activeCovid, setActiveCovid] = useState(null);
+  /* const covidIcon = new Icon({
+    iconUrl: icon1,
+    iconSize: [25, 25]
+  }) */
+  return (
+    <MapContainer center={[20.593683, 78.962883]} zoom={5}>
+      <TileLayer
+        attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+        url='https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png'
+      />
+      {covidData.map(eachData => (
+        <Marker
+          key={eachData.Id}
+          position={[eachData.Latitude, eachData.Longitude]}
+          eventHandlers={{
+            click: () => {
+              setActiveCovid(eachData)
+            }
+          }}
+        />
+      ))}
+      {activeCovid && (
+        <Popup
+          position={[activeCovid.Latitude, activeCovid.Longitude]}
+          onClose={() => {
+            setActiveCovid(null)
+          }}
+        >
+          <div>
+            <h1>{activeCovid.Location}</h1>
+            <p>Total cases:  {activeCovid.Total_Cases}</p>
+            <p>New cases (1 day*):  {activeCovid.New_Cases_Per_Day}</p>
+            <p>Cases per 1 million people:  {activeCovid.Cases_Per_1_Million_People}</p>
+            <p>Deaths:  {activeCovid.Deaths}</p>
+          </div>
+        </Popup>
+      )}
+    </MapContainer>
+  )
+}
 export default App;
